80001978f4a73b1f2d99c997c520f2fe1cad7cdc
Sir-Morara <moraragift@gmail.com>
2024-08-16 00:13:00 +0300

    Made necessary changes

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,7 +1,14 @@
 let books = {
-      1: { "isbn": "978-1-56619-909-4", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {} },
-      2: { "isbn": "978-0-14-303995-2", "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
-      3: { "isbn": "978-0-14-044912-4", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
+      1: { "isbn": "978-1-56619-909-4", "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {
+        "user1": "An insightful and powerful novel.",
+        "user2": "A classic that provides a deep cultural perspective."
+      }},
+      2: { "isbn": "978-0-14-303995-2", "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {
+        "user3": "Charming and imaginative stories for all ages."
+      }},
+      3: { "isbn": "978-0-14-044912-4", "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {
+        "user4": "A profound exploration of the human condition."
+      }},
       4: { "isbn": "978-1-56619-910-1", "author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {} },
       5: { "isbn": "978-1-56619-911-8", "author": "Unknown", "title": "The Book Of Job", "reviews": {} },
       6: { "isbn": "978-1-56619-912-5", "author": "Unknown", "title": "One Thousand and One Nights", "reviews": {} },
